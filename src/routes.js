c4d99a86d7d4965e0bd7cd39eb57df773574b849
Caio Rodrigues <caiorodrigues@hotmail.com>
2020-01-02 17:42:41 -0300

    [DEV] Class10 - Creating Models loader

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello world' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Caio Rodrigues',
+    email: 'caio@email.com',
+    password_hash: '123123',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
